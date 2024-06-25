d54b7b07fef43a871ee68244820292602b211b7a
sbx_user1051 <sbx_user1051@169.254.56.77>
2024-06-25 13:08:53 +0000

    Tue Jun 25 13:08:53 UTC 2024

---
+++ test/solutions/HLO/test-hlo.js
@@ -0,0 +1,12 @@
+var mocha = require('mocha')
+var describe = mocha.describe
+var it = mocha.it
+var assert = require('assert');
+const hello = require('../../../lib/solutions/HLO/hello');
+
+describe('Hello world Challenge: says hello to the world', function () {
+	it('should return Hello World!', function () {
+    const friendName = "John";
+		assert.equal(hello(friendName), "Hello World!");
+	});
+});
