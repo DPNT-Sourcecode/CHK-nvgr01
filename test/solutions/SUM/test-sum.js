0406396f620d91e578bc10bae5455320036783d9
sbx_user1051 <sbx_user1051@169.254.56.77>
2024-06-25 12:58:42 +0000

    Tue Jun 25 12:58:42 UTC 2024

--- test/solutions/SUM/test-sum.js
+++ test/solutions/SUM/test-sum.js
@@ -8,4 +8,8 @@ describe('SUM challenge: adding two numbers', function() {
 	it('should return 3, which is the sum of 1 and 2', function() {
 	    assert.equal(sum(1, 2), 3);
 	});
+
+	it('should throw an error if less than zero', function() {
+		assert.throwa(() => sum(-1, 2), Error, "x and y must be between 0 and 100");
+});
 });
