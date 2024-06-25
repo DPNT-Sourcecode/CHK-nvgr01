cc0fdb345c36147409c3ff0ceaa839fa92ed9c77
sbx_user1051 <sbx_user1051@169.254.56.77>
2024-06-25 13:01:10 +0000

    Tue Jun 25 13:01:10 UTC 2024

--- test/solutions/SUM/test-sum.js
+++ test/solutions/SUM/test-sum.js
@@ -4,12 +4,17 @@ var it = mocha.it
 var assert = require('assert');
 const sum = require('../../../lib/solutions/SUM/sum');
 
-describe('SUM challenge: adding two numbers', function() {
-	it('should return 3, which is the sum of 1 and 2', function() {
-	    assert.equal(sum(1, 2), 3);
+describe('SUM challenge: adding two numbers', function () {
+	it('should return 3, which is the sum of 1 and 2', function () {
+		assert.equal(sum(1, 2), 3);
 	});
 
-	it('should throw an error if less than zero', function() {
-		assert.throwa(() => sum(-1, 2), Error, "x and y must be between 0 and 100");
+	it('should throw an error if a value less than zero', function () {
+		assert.throws(() => sum(-1, 2), Error, "x and y must be between 0 and 100");
+	});
+
+	it('should throw an error if a value is greater than 100', function () {
+		assert.throws(() => sum(1, 200), Error, "x and y must be between 0 and 100");
+	});
 });
-});
+
