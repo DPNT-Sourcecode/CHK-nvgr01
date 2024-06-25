b9d01a3f1fa2d040639a2284dd9bbdc6f1c16440
sbx_user1051 <sbx_user1051@169.254.56.77>
2024-06-25 13:30:20 +0000

    Tue Jun 25 13:30:20 UTC 2024

--- test/solutions/CHK/test-chk.js
+++ test/solutions/CHK/test-chk.js
@@ -4,5 +4,17 @@ var it = mocha.it
 var assert = require('assert');
 const checkout = require('../../../lib/solutions/CHK/checkout');
 
-describe('', function () {
+describe('Checkout Challenge: returns the total basket value', function () {
+  it('should return the total value of the basket', function () {
+    const skus = "ABCD";
+    const result = checkout(skus);
+    assert.equal(result, 115);
+	});
+
+  it('should return -1', function () {
+    const skus = "ABCDE";
+    const result = checkout(skus);
+    assert.equal(result, 115);
+	});
 });
+
