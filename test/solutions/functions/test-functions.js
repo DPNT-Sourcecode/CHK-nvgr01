edda326f4f6d0f5a7dbf3bd0d4f32248fe774dba
sbx_user1051 <sbx_user1051@169.254.56.77>
2024-06-25 19:53:30 +0000

    Tue Jun 25 19:53:30 UTC 2024

--- test/solutions/functions/test-functions.js
+++ test/solutions/functions/test-functions.js
@@ -6,9 +6,41 @@ const { getAllDiscountOrders, getMinMaxDiscount, calculateBestDiscount, initiali
 
 describe('Test key functions', function () {
 	it('should test the checkForBulkDiscount', function () {
-    const obj = {
-      "S": 0
+    let obj = {
+      "S": 1,
+      "T": 1,
+      "Y": 1,
+      "X": 1,
+      "Z": 1
     };
 		assert.equal(checkForBulkDiscount(obj), true);
+
+    obj = {
+      "S": 0,
+      "T": 0,
+      "Y": 0,
+      "X": 1,
+      "Z": 1
+    };
+    assert.equal(checkForBulkDiscount(obj), false);
+	});
+
+  it('should test initializeObjectFromArray', function () {
+    const arr = ["A", "B", "C"];
+
+    const obj = initializeObjectFromArray(arr, 0);
+    assert.deepEqual(obj, {
+      "A": 0,
+      "B": 0,
+      "C": 0
+    });
+
+    const obj2 = initializeObjectFromArray(arr, false);
+    assert.deepEqual(obj2, {
+      "A": false,
+      "B": false,
+      "C": false
+    });
 	});
 });
+
